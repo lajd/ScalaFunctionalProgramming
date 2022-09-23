cf6f6a8f834fa8b87adb30914fe3275f4858cc32
Jonathan La <jon.ladavid@gmail.com>
2022-09-23 12:08:27 -0700

    Add a4 huffman coding project

---
+++ assignments/a4_patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,70 @@
+package patmat
+
+class HuffmanSuite extends munit.FunSuite:
+  import Huffman.*
+
+  trait TestTrees {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val sampleTree = makeCodeTree(
+      makeCodeTree(
+        Leaf('x', 1),
+        Leaf('e', 1)
+      ),
+      Leaf('t', 2)
+    )
+  }
+
+
+  test("weight of a larger tree (10pts)") {
+    new TestTrees:
+      assertEquals(weight(t1), 5)
+  }
+
+
+  test("chars of a larger tree (10pts)") {
+    new TestTrees:
+      assertEquals(chars(t2), List('a','b','d'))
+  }
+
+  test("test create sampleTree") {
+    new TestTrees:
+      assertEquals(
+        weight(sampleTree), 4
+      )
+  }
+
+  test("string2chars hello world") {
+    assertEquals(string2Chars("hello, world"), List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+
+  test("make ordered leaf list for some frequency table (15pts)") {
+    assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+  }
+
+
+  test("combine of some leaf list (15pts)") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assertEquals(combine(leaflist), List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+  }
+
+  test("decode and encode a very short text should be identity (10pts)") {
+    new TestTrees:
+      assertEquals(decode(t1, encode(t1)("ab".toList)), "ab".toList)
+  }
+
+  test("convert to CodeTable") {
+    new TestTrees:
+
+      assertEquals(
+        convert(sampleTree),
+        List(
+          ('x', List(0, 0)),
+          ('e', List(0, 1)),
+          ('t', List(1)),
+        )
+      )
+  }
+  import scala.concurrent.duration.*
+  override val munitTimeout = 10.seconds
